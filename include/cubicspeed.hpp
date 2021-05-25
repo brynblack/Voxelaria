a34ea472ef85977f9dfa4c00bf75915c4cc28ab4
brynblack <49110391+brynblack@users.noreply.github.com>
2021-05-26 07:41:21 +1000

    Major changes to game, added 3D and more

--- include/cubicspeed.hpp
+++ include/cubicspeed.hpp
@@ -1,56 +1,41 @@
 #ifndef cubicspeedlibs
 #define cubicspeedlibs
+#include <glm/ext/vector_float3.hpp>
 namespace cubicSpeed
 {
-	class entity {
-	public:
-		float size;
-		float rgba[4];
-		float speed;
-		float xy[2];
-		float dxy[2];
-		float dist;
-	public:
-		void set_size(float);
-		void set_rgba(float, float, float, float);
-		void set_xy(float, float);
-		void set_speed(float);
-	};
-	void entity::set_size(float s)
-	{
-		size = s;
-	};
-	void entity::set_rgba(float r, float g, float b, float a)
-	{
-		rgba[0] = r;
-		rgba[1] = g;
-		rgba[2] = b;
-		rgba[3] = a;
-	};
-	void entity::set_speed(float s)
-	{
-		speed = s;
-	};
-	void entity::set_xy(float x, float y)
-	{
-		xy[0] = x;
-		xy[1] = y;
-	};
-	float *ortho(float left, float right, float bottom, float top, float near, float far)
-	{
-		static float matrix[4][4];
-		matrix[0][0] = 2.0f / (right - left);
-		matrix[1][1] = 2.0f / (top - bottom);
-		matrix[2][2] = -2.0f / (far - near);
-		matrix[3][0] = -(right + left) / (right - left);
-		matrix[3][1] = -(top + bottom) / (top - bottom);
-		matrix[3][2] = -(far + near) / (far - near);
-		matrix[3][3] = 1.0f;
-		return *matrix;
-	};
-	struct properties {
-		int width, height;
-		const char *title;
-	};
+    class entity {
+    public:
+        glm::vec3 xyz;
+        double pitch;
+        double yaw;
+        glm::vec3 direction;
+        float speed;
+    public:
+        void set_xyz(glm::vec3);
+        void set_pitch(double);
+        void set_yaw(double);
+        void set_direction(glm::vec3);
+        void set_speed(float);
+    };
+    void entity::set_xyz(glm::vec3 XYZ)
+    {
+        xyz = XYZ;
+    };
+    void entity::set_pitch(double p)
+    {
+        pitch = p;
+    };
+    void entity::set_yaw(double y)
+    {
+        yaw = y;
+    };
+    void entity::set_direction(glm::vec3 d)
+    {
+        direction = d;
+    };
+    void entity::set_speed(float s)
+    {
+        speed = s;
+    };
 };
 #endif
--- src/cubicspeed.cpp
+++ src/cubicspeed.cpp
@@ -1,210 +1,250 @@
-#include "cubicspeed.hpp"
+// OpenGL Headers
 #include <GLEW/glew.h>
 #include <GLFW/glfw3.h>
-#include <math.h>
+
+// Utility Headers
+#include "cubicspeed.hpp"
 #include <chrono>
 #include <string>
 #include <iostream>
+
+// GLM Headers
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+struct properties {
+    int width, height;
+    const char *title;
+};
 
 GLFWmonitor* monitor;
 GLFWvidmode* mode;
 GLFWwindow* window;
 
-cubicSpeed::entity player, enemy, goal;
-cubicSpeed::properties GLwindow;
+cubicSpeed::entity camera;
+properties GLwindow;
 
-std::chrono::high_resolution_clock::time_point t1, t2;
+glm::vec3 cameraFront, cameraUp;
 
-int score;
+double lastX = GLwindow.width / 2.0;
+double lastY = GLwindow.height / 2.0;
+float fov = 45.0f;
+bool firstMouse = true;
+
 float frameDelta;
 
+unsigned int indicies[] = {
+        0, 1, 2,
+        2, 3, 0
+};
+
 unsigned int buffer;
+unsigned int ibo;
 unsigned int shader;
-
-void config()
-{
-	player.set_size(16.0f);
-	player.set_rgba(1.0f, 1.0f, 0.0f, 1.0f);
-	player.set_speed(1.0f);
-	player.set_xy(0.0f, 0.0f);
-
-	enemy.set_size(16.0f);
-	enemy.set_rgba(1.0f, 0.0f, 0.0f, 1.0f);
-	enemy.set_speed(0.1f);
-	enemy.set_xy((float)GLwindow.width, (float)GLwindow.height);
-
-	goal.set_size(64.0f);
-	goal.set_rgba(0.0f, 1.0f, 0.0f, 1.0f);
-	goal.set_xy(float(rand() % GLwindow.width), float(rand() % GLwindow.height));
-}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
-	unsigned int id = glCreateShader(type);
-	const char* src = source.c_str() ;
-	glShaderSource(id, 1, &src, nullptr);
-	glCompileShader(id);
-	int result;
-	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-	if (result == GL_FALSE)
-	{
-		int length;
-		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-		char* message = (char*)alloca(length * sizeof(char));
-		glGetShaderInfoLog(id, length, &length, message);
-		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader" << std::endl;
-		std::cout << message << std::endl;
-		glDeleteShader(id);
-		return 0;
-	}
-	return id;
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str() ;
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+    return id;
 }
 
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
-	unsigned int program = glCreateProgram();
-	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-	glAttachShader(program, vs);
-	glAttachShader(program, fs);
-	glLinkProgram(program);
-	glValidateProgram(program);
-	glDeleteShader(vs);
-	glDeleteShader(fs);
-	return program;
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    return program;
 }
 
-int init()
+void mouse_callback(GLFWwindow* wwindow, double xpos, double ypos)
 {
-	if (!glfwInit())
-		return -1;
-	monitor = glfwGetPrimaryMonitor();
-	mode = (GLFWvidmode*)glfwGetVideoMode(monitor);
-	GLwindow.width = mode->width;
-	GLwindow.height = mode->height;
-	window = glfwCreateWindow(GLwindow.width, GLwindow.height, "OpenGL", monitor, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	if (glewInit() != GLEW_OK)
-	{
-		glfwTerminate();
-		return -1;
-	}
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (GLvoid*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (GLvoid*)(2 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (GLvoid*)(6 * sizeof(float)));
-	glEnable(GL_PROGRAM_POINT_SIZE);
-	std::string vertexShader = "#version 330 core\n"
-		"layout (location = 0) in vec2 position;\n"
-		"layout (location = 1) in vec4 color;\n"
-		"layout (location = 2) in float size;\n"
-		"uniform mat4 projection;\n"
-		"out vec4 objColor;\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = projection * vec4(position, 0.0, 1.0);\n"
-		"	objColor = color;\n"
-		"	gl_PointSize = size;\n"
-		"}\0";
-	std::string fragmentShader = "#version 330 core\n"
-		"in vec4 objColor;\n"
-		"out vec4 color;\n"
-		"void main()\n"
-		"{\n"
-		"	color = objColor;\n"
-		"}\0";
-	shader = CreateShader(vertexShader, fragmentShader);
-	glUseProgram(shader);
-	return 0;
-}
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
 
-int input()
-{
-	glfwPollEvents();
-	if (glfwGetKey(window, GLFW_KEY_A) && !(player.xy[0] < 0.0f))
-		player.xy[0] -= player.speed * frameDelta;
-	if (glfwGetKey(window, GLFW_KEY_D) && !(player.xy[0] > (float)GLwindow.width))
-		player.xy[0] += player.speed * frameDelta;
-	if (glfwGetKey(window, GLFW_KEY_S) && !(player.xy[1] < 0.0f))
-		player.xy[1] -= player.speed * frameDelta;
-	if (glfwGetKey(window, GLFW_KEY_W) && !(player.xy[1] > (float)GLwindow.height))
-		player.xy[1] += player.speed * frameDelta;
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE))
-	{
-		glfwTerminate();
-		return 1;
-	}
-	return 0;
-}
+    double xoffset = xpos - lastX;
+    double yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
 
-void calculate()
-{
-	enemy.dxy[0] = player.xy[0] - enemy.xy[0];
-	enemy.dxy[1] = player.xy[1] - enemy.xy[1];
-	enemy.dist = sqrt((enemy.dxy[0] * enemy.dxy[0]) + (enemy.dxy[1] * enemy.dxy[1]));
-	enemy.dxy[0] /= enemy.dist;
-	enemy.dxy[1] /= enemy.dist;
-	enemy.xy[0] += enemy.dxy[0] * enemy.speed * frameDelta;
-	enemy.xy[1] += enemy.dxy[1] * enemy.speed * frameDelta;
-	goal.dxy[0] = player.xy[0] - goal.xy[0];
-	goal.dxy[1] = player.xy[1] - goal.xy[1];
-	goal.dist = sqrt((goal.dxy[0] * goal.dxy[0]) + (goal.dxy[1] * goal.dxy[1]));
-	if (enemy.dist < ((player.size + enemy.size) / 2.0f))
-	{
-		config();
-		score = 0;
-	}
-	if (goal.dist < ((player.size + goal.size) / 2.0f))
-	{
-		score += 1;
-		goal.set_xy(float(rand() % GLwindow.width), float(rand() % GLwindow.height));
-		enemy.set_speed(enemy.speed + 0.1f);
-		enemy.set_size(enemy.size + 0.1f);
-	}
-}
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
 
-void draw(float *matrix, int transform_location)
-{
-	float verticies[21] = {
-		player.xy[0], player.xy[1], player.rgba[0], player.rgba[1], player.rgba[2], player.rgba[3], player.size,
-		enemy.xy[0], enemy.xy[1], enemy.rgba[0], enemy.rgba[1], enemy.rgba[2], enemy.rgba[3], enemy.size,
-		goal.xy[0], goal.xy[1], goal.rgba[0], goal.rgba[1], goal.rgba[2], goal.rgba[3], goal.size
-	};
-	glUniformMatrix4fv(transform_location, 1, GL_FALSE, matrix);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 21 * sizeof(float), verticies, GL_DYNAMIC_DRAW);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glDrawArrays(GL_POINTS, 0, 3);
-	glFlush();
-	glfwSwapBuffers(window);
+    camera.yaw   += xoffset;
+    camera.pitch += yoffset;
+
+    if(camera.pitch > 89.0f)
+        camera.pitch = 89.0f;
+    if(camera.pitch < -89.0f)
+        camera.pitch = -89.0f;
+
+    camera.direction.x = cos(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
+    camera.direction.y = sin(glm::radians(camera.pitch));
+    camera.direction.z = sin(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
+    cameraFront = glm::normalize(camera.direction);
 }
 
 int main()
 {
-	if (init() == -1)
-		return -1;
-	config();
-	float *matrix = cubicSpeed::ortho(0.0f, (float)GLwindow.width, 0.0f, (float)GLwindow.height, 0.0f, 1.0f);
-	int transform_location = glGetUniformLocation(shader, "projection");
-	while (!glfwWindowShouldClose(window))
-	{
-		frameDelta = std::chrono::duration<float, std::milli>(t2 - t1).count(); // Game speed modifier
-		t1 = std::chrono::high_resolution_clock::now();
-		if (input() == 1)
-			return 0;
-		calculate();
-		draw(matrix, transform_location);
-		t2 = std::chrono::high_resolution_clock::now();
-	}
-	glfwTerminate();
-	return 0;
+    std::chrono::high_resolution_clock::time_point t1, t2; // Define time point variables
+
+    // Init glfw
+    if (!glfwInit())
+        return -1;
+    monitor = glfwGetPrimaryMonitor();
+    mode = (GLFWvidmode*)glfwGetVideoMode(monitor);
+    GLwindow.width = mode->width;
+    GLwindow.height = mode->height;
+    window = glfwCreateWindow(GLwindow.width, GLwindow.height, "CubicSpeed", monitor, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Configure mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid*)nullptr);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid*)(3 * sizeof(float)));
+
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+    glEnable(GL_BLEND);
+    glEnable(GL_DEPTH_TEST);
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    std::string vertexShader = "#version 330 core\n"
+                               "layout (location = 0) in vec3 v_pos;\n"
+                               "layout (location = 1) in vec3 v_color;\n"
+                               "uniform mat4 model;\n"
+                               "uniform mat4 view;\n"
+                               "uniform mat4 projection;\n"
+                               "out vec4 vs_color;\n"
+                               "void main()\n"
+                               "{\n"
+                               "	gl_Position = projection * view * model * vec4(v_pos, 1.0);\n"
+                               "	vs_color = vec4(v_color, 1.0);\n"
+                               "}";
+    std::string fragmentShader = "#version 330 core\n"
+                                 "in vec4 vs_color;\n"
+                                 "out vec4 fs_color;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "	fs_color = vs_color;\n"
+                                 "}";
+    shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
+    // Set camera settings
+    camera.set_xyz(glm::vec3(0.0f, 0.0f,  3.0f));
+    camera.set_pitch(0.0);
+    camera.set_yaw(-90.0);
+    camera.set_speed(0.01f);
+
+    cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    cameraUp = glm::vec3(0.0f, 1.0f,  0.0f);
+
+    // Declare matrices
+    glm::mat4 Model = glm::mat4(1.0f);
+    //Model = glm::rotate(Model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    glm::mat4 View = glm::mat4(1.0f);
+    glm::mat4 Projection = glm::perspective(glm::radians(90.0f), (GLfloat)GLwindow.width/(GLfloat)GLwindow.height, 0.1f, 100.0f);
+
+    // Get matrix locations
+    int model_location = glGetUniformLocation(shader, "model");
+    int view_location = glGetUniformLocation(shader, "view");
+    int projection_location = glGetUniformLocation(shader, "projection");
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Calculate time delta
+        frameDelta = std::chrono::duration<float, std::milli>(t2 - t1).count(); // Game speed modifier
+        t1 = std::chrono::high_resolution_clock::now(); // Get first time point
+
+        // Input stuff
+        glfwPollEvents();
+        if (glfwGetKey(window, GLFW_KEY_A))
+            camera.xyz -= glm::normalize(glm::cross(cameraFront, cameraUp)) * camera.speed * frameDelta;
+        if (glfwGetKey(window, GLFW_KEY_D))
+            camera.xyz += glm::normalize(glm::cross(cameraFront, cameraUp)) * camera.speed * frameDelta;
+        if (glfwGetKey(window, GLFW_KEY_S))
+            camera.xyz -= camera.speed * cameraFront * frameDelta;
+        if (glfwGetKey(window, GLFW_KEY_W))
+            camera.xyz += camera.speed * cameraFront * frameDelta;
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+        {
+            glfwTerminate();
+            return 0;
+        }
+
+        // Draw everything
+        float verticies[24] = {
+                0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+                0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+                -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+                -0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 0.0f
+        };
+
+        View = glm::lookAt(camera.xyz, camera.xyz + cameraFront, cameraUp);
+
+        // Send matrices to shader
+        glUniformMatrix4fv(model_location, 1, GL_FALSE, glm::value_ptr(Model));
+        glUniformMatrix4fv(view_location, 1, GL_FALSE, glm::value_ptr(View));
+        glUniformMatrix4fv(projection_location, 1, GL_FALSE, glm::value_ptr(Projection));
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, 24 * sizeof(float), verticies, GL_DYNAMIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indicies, GL_DYNAMIC_DRAW);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glFlush();
+        glfwSwapBuffers(window);
+
+        t2 = std::chrono::high_resolution_clock::now(); // Get second time point
+    }
+    glfwTerminate();
+    return 0;
 }

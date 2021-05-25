d99058017d3653b47a2f10bade965f9904c3407f
brynblack <49110391+brynblack@users.noreply.github.com>
2021-05-26 07:55:20 +1000

    Added many comments

--- src/cubicspeed.cpp
+++ src/cubicspeed.cpp
@@ -13,36 +13,43 @@
 #include <glm/mat4x4.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// Define properties struct which holds variables for window
 struct properties {
     int width, height;
     const char *title;
 };
 
+// Declare GLFW variables
 GLFWmonitor* monitor;
 GLFWvidmode* mode;
 GLFWwindow* window;
 
+// Declare camera and window variables
 cubicSpeed::entity camera;
+glm::vec3 cameraFront, cameraUp;
 properties GLwindow;
 
-glm::vec3 cameraFront, cameraUp;
-
+// Declare mouse variables
 double lastX = GLwindow.width / 2.0;
 double lastY = GLwindow.height / 2.0;
 float fov = 45.0f;
 bool firstMouse = true;
 
+// Declare frameDelta variable used to calculate time delta
 float frameDelta;
 
+// Define element buffer array
 unsigned int indicies[] = {
         0, 1, 2,
         2, 3, 0
 };
 
+// Define buffer variables
 unsigned int buffer;
 unsigned int ibo;
 unsigned int shader;
 
+// Compile shader function to compile the given shader
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
     unsigned int id = glCreateShader(type);
@@ -65,6 +72,7 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
     return id;
 }
 
+// Create shader function to generate a shader
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
     unsigned int program = glCreateProgram();
@@ -79,6 +87,7 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
     return program;
 }
 
+// Function to process mouse input
 void mouse_callback(GLFWwindow* wwindow, double xpos, double ypos)
 {
     if (firstMouse)
@@ -100,17 +109,20 @@ void mouse_callback(GLFWwindow* wwindow, double xpos, double ypos)
     camera.yaw   += xoffset;
     camera.pitch += yoffset;
 
+    // Lock camera angles
     if(camera.pitch > 89.0f)
         camera.pitch = 89.0f;
     if(camera.pitch < -89.0f)
         camera.pitch = -89.0f;
 
+    // Set camera direction based upon yaw and pitch
     camera.direction.x = cos(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
     camera.direction.y = sin(glm::radians(camera.pitch));
     camera.direction.z = sin(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
     cameraFront = glm::normalize(camera.direction);
 }
 
+// Main function to initialise OpenGL and run all other code
 int main()
 {
     std::chrono::high_resolution_clock::time_point t1, t2; // Define time point variables
@@ -139,6 +151,8 @@ int main()
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwSetCursorPosCallback(window, mouse_callback);
 
+    // Generate buffers and configure vertex attributes
+    // Array buffer
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glEnableVertexAttribArray(0);
@@ -146,14 +160,19 @@ int main()
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid*)(3 * sizeof(float)));
 
+    // Element buffer
     glGenBuffers(1, &ibo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 
+    // Enable OpenGL blending and depth testing
     glEnable(GL_BLEND);
     glEnable(GL_DEPTH_TEST);
 
+    // Set OpenGl blending function
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    // Main vertex and fragment shader code
+    // TODO: Add code that allows shader code to be imported from an external file
     std::string vertexShader = "#version 330 core\n"
                                "layout (location = 0) in vec3 v_pos;\n"
                                "layout (location = 1) in vec3 v_color;\n"
@@ -173,6 +192,7 @@ int main()
                                  "{\n"
                                  "	fs_color = vs_color;\n"
                                  "}";
+    // Create shader program
     shader = CreateShader(vertexShader, fragmentShader);
     glUseProgram(shader);
 
@@ -219,8 +239,8 @@ int main()
             return 0;
         }
 
-        // Draw everything
-        float verticies[24] = {
+        // Define vertices
+        float vertices[24] = {
                 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
                 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
                 -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
@@ -234,17 +254,23 @@ int main()
         glUniformMatrix4fv(view_location, 1, GL_FALSE, glm::value_ptr(View));
         glUniformMatrix4fv(projection_location, 1, GL_FALSE, glm::value_ptr(Projection));
 
+        // Bind buffers and set buffer data
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, 24 * sizeof(float), verticies, GL_DYNAMIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, 24 * sizeof(float), vertices, GL_DYNAMIC_DRAW);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indicies, GL_DYNAMIC_DRAW);
+
+        // Clear buffers and draw element buffer
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        // Flush OpenGl and swap buffers
         glFlush();
         glfwSwapBuffers(window);
 
         t2 = std::chrono::high_resolution_clock::now(); // Get second time point
     }
+    // Terminate program if main loop is exited
     glfwTerminate();
     return 0;
 }

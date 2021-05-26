7c2b7180829c872700c2e0a26cec799c01b98d01
brynblack <49110391+brynblack@users.noreply.github.com>
2021-05-26 16:21:32 +1000

    Cleaned up header file

--- include/cubicspeed.hpp
+++ include/cubicspeed.hpp
@@ -5,37 +5,45 @@ namespace cubicSpeed
 {
     class entity {
     public:
-        glm::vec3 xyz;
-        double pitch;
-        double yaw;
-        glm::vec3 direction;
+        glm::vec3 xyz, front, up, direction;
+        double pitch, yaw;
         float speed;
     public:
         void set_xyz(glm::vec3);
         void set_pitch(double);
         void set_yaw(double);
+        void set_front(glm::vec3);
+        void set_up(glm::vec3);
         void set_direction(glm::vec3);
         void set_speed(float);
     };
     void entity::set_xyz(glm::vec3 XYZ)
     {
         xyz = XYZ;
-    };
+    }
     void entity::set_pitch(double p)
     {
         pitch = p;
-    };
+    }
     void entity::set_yaw(double y)
     {
         yaw = y;
-    };
+    }
+    void entity::set_front(glm::vec3 f)
+    {
+        front = f;
+    }
+    void entity::set_up(glm::vec3 u)
+    {
+        up = u;
+    }
     void entity::set_direction(glm::vec3 d)
     {
         direction = d;
-    };
+    }
     void entity::set_speed(float s)
     {
         speed = s;
-    };
-};
+    }
+}
 #endif
